45437b503c2f8806da03667acd5f4e80757f82b3
khalid <khalidmiah@hotmail.co.uk>
2021-10-12 16:59:31 +0100

    interacting with postgres from ruby - tests passed

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -5,9 +5,10 @@ describe Bookmark do
     it 'returns all bookmarks' do
       bookmarks = Bookmark.all
 
-      expect(bookmarks).to include("http://www.makersacademy.com")
-      expect(bookmarks).to include("http://www.destroyallsoftware.com")
+      expect(bookmarks).to include("http://www.askjeeves.com")
+      expect(bookmarks).to include("http://www.twitter.com")
       expect(bookmarks).to include("http://www.google.com")
+  
     end
   end
 end
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -2,8 +2,8 @@ feature "Viewing bookmarks at /bookmarks" do
     scenario "User wants to views their bookmarks" do
         visit "/bookmarks"
         
-        expect(page).to have_content "http://www.makersacademy.com"
-        expect(page).to have_content "http://www.destroyallsoftware.com"
+        expect(page).to have_content "http://www.askjeeves.com"
+        expect(page).to have_content "http://www.twitter.com"
         expect(page).to have_content "http://www.google.com"
     end
 
