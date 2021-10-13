5d48a7a7870e2a80c8eb90ef02f9b0bc363ce28f
khalid <khalidmiah@hotmail.co.uk>
2021-10-13 13:46:03 +0100

    bookmark manager wednesday update

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -5,6 +5,7 @@ feature "Viewing bookmarks at /bookmarks" do
         expect(page).to have_content "http://www.askjeeves.com"
         expect(page).to have_content "http://www.twitter.com"
         expect(page).to have_content "http://www.google.com"
+        
     end
 
 end
